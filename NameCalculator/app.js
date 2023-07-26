ed946df59ca9181585b7f36db432ac47cc7001a0
Lakshmi Tejashree Korivi <tejashreekorivi@gmail.com>
2023-07-26 11:40:54 -0500

    Name Calculator SPA

---
+++ NameCalculator/app.js
@@ -0,0 +1,27 @@
+var student = {
+  name: "",
+  type: "student"
+};
+
+document.addEventListener('DOMContentLoaded', contentLoaded);
+
+function contentLoaded(event) {
+  document.getElementById('name').addEventListener("keyup", keyUp);
+}
+
+function keyUp(event) {
+  calculateNumericOutput ();
+}
+
+function calculateNumericOutput () {
+  user.name = document.getElementById('name').value;
+
+  var totalNameValue = 0;
+  for (var i = 0; i < user.name.length; i++) {
+    totalNameValue += user.name.charCodeAt(i);
+  }
+
+  //To insert result into the web page
+  var output = "Total Numeric value of the name is " + totalNameValue;
+  document.getElementById('output').innertext = output;
+}
