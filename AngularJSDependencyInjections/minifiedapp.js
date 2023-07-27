af6177ba6b6900032d34c82b002580a8f0981ca4
Lakshmi Tejashree Korivi <tejashreekorivi@gmail.com>
2023-07-27 13:26:09 -0500

    AngularJS Dependency Injections

---
+++ AngularJSDependencyInjections/minifiedapp.js
@@ -0,0 +1,3 @@
+
+
+!function(){"use strict";function e(e,n,r){e.name="Tejashree",e.upper=function(){var r=n("uppercase");e.name=r(e.name)}}angular.module("DIApp",[]).controller("DIController",e),e.$inject=["$scope","$filter"]}();
