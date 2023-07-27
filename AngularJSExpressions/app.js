292b38b8f7c699f7518e91bc483f4a5c656cadf7
Lakshmi Tejashree Korivi <tejashreekorivi@gmail.com>
2023-07-27 15:12:30 -0500

    AngularJS Expressionsmodifications

--- AngularJSExpressions/app.js
+++ AngularJSExpressions/app.js
@@ -7,9 +7,13 @@
   MsgController.$inject = ['$scope'];
   function MsgController($scope) {
     $scope.name = "Tejashree";
-
+    $scope.stateOfBeing = "doubtful";
     $scope.sayMessage = function () {
       return "I like to read at night!";
+    };
+
+    $scope.giveBook = function functionName() {
+      $scope.stateOfBeing = "happy";
     };
   }
 
